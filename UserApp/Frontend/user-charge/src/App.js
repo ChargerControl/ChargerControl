261837dc7970a53335d2bd47c286325bd497f7d2
costinha03 <dmcosta03@ua.pt>
2025-05-16 17:37:09 +0100

    login and register pages

--- UserApp/Frontend/user-charge/src/App.js
+++ UserApp/Frontend/user-charge/src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Components/Authentication/login';
+import Register from './Components/Authentication/register';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={
+            <header className="App-header">
+              <p>Welcome to ChargerControl</p>
+            </header>
+          } />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
